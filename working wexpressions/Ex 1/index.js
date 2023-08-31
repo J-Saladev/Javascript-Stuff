21a97bd1fedc50485a332a7a36da8544952b83f6
navymrc <navymrc@gmail.com>
2023-08-31 13:19:53 -0400

    Finished Exercise 1

--- working wexpressions/Ex 1/index.js
+++ working wexpressions/Ex 1/index.js
@@ -4,9 +4,31 @@ let SB = document.getElementById("Side B");
 
 let SC = document.getElementById("Side C");
 
-
 function calc() {
-    let p = (parseInt(SA.value) + parseInt(SB.value) + parseInt(SC.value)) / 2;
-    console.log (p);
-    document.getElementById("answer").innerHTML = p;
+    // VVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVVV
+    
+
+    let a = parseInt(SA.value);
+    let b = parseInt(SB.value);
+    let c = parseInt(SC.value);
+// ^^^^^^^^^^^^^^^^^^^^^^^^^^^^This defining and turning the input into variables^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    
+    let p = (a + b + c)/ 2;
+    console.log ("p=", p);
+    let pa = p - a;
+    let pb = p - b;
+    let pc = p - c;
+    console.log("p-a=", pa )
+    console.log("p-b=", pb)
+    console.log("p-c", pc)
+
+    let sqr1 = Math.sqrt(p * pa * pb * pc);
+    console.log(sqr1)
+    
+
+    
+    document.getElementById("answer").innerHTML= sqr1;
+    
 }
+
+
