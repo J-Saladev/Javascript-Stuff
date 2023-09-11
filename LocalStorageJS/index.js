abd64c6cd1d70249af72d08e1773be242a212686
Jack <jackdantwilder@gmail.com>
2023-09-11 10:51:47 -0400

    Finished pizza stuff

--- LocalStorageJS/index.js
+++ LocalStorageJS/index.js
@@ -1,128 +1,31 @@
-const user = JSON.parse(localStorage.getItem("user"));
-function submitheInfo() {
+
+function submitheInfo(e) {
+    e.preventDefault();
     localStorage.clear();
+   
+    
+    const form = document.getElementById("form");
+    if (!form.checkValidity()) {
+        form.classList.add("was-validated");
+        form.reportValidity();
+        
+        return;
+  
+    }
+   
+
     let firstname = document.querySelector("#firstname").value;
     let lastname = document.querySelector("#lastname").value;
     let type = document.querySelector("#type").value;
     let pickup = document.querySelector("#pickupdate").value;
     let dropoff = document.querySelector("#returndate").value;
-
-    if (firstname && lastname && type !== "Choose..." && pickup && dropoff && pickup < dropoff) {
-        let user = {
-            firstname,
-            lastname,
-            type,
-            pickup,
-            dropoff
-        }
-        localStorage.setItem("user", JSON.stringify(user));
-        window.location.href = "results.html";
-    } else if (dropoff < pickup) {
-        alert("Please check your dates");
-    } else {
-        alert("Please fill out all fields");
+ let user = {
+       firstname,
+       lastname,
+       type,
+       pickup,
+       dropoff
+      }
+       localStorage.setItem("user", JSON.stringify(user));
+       window.location.href = "results.html";
     }
-}
-
-function processResult() {
-    showResult();
-    finalPrice();
-    showMess();
-}
-
-function showResult() {
-    let user = JSON.parse(localStorage.getItem("user"));
-    console.log(user);
-    let type = user.type;
-    let pickup = user.pickup;
-    let dropoff = user.dropoff;
-    let ppday = "";
-    switch (type) {
-        case "lux":
-            ppday = 1000;
-            type = " Luxury Vehicle,";
-            break;
-        case "sport":
-            ppday = 500;
-            type = " Sports Vehicle, ";
-            type 
-            break;
-        case "util":
-            ppday = 200;
-            type = "n Utility Vehicle,";
-            break;
-        case "econ":
-            ppday = 100;
-            type = "n Economy Vehicle, ";
-            break;
-        default:
-            alert("error");
-            break;
-        
-    }
-    
-    localStorage.setItem("ppday", ppday);
-    localStorage.setItem("type", type);
-    console.log("Show Result Working");
-}
-
-function toDate(a) {
-    let parts = a.split("-");
-    return new Date(parts[0], parts[1] - 1, parts[2]);
-}
-
-function finalPrice() {
-    let ppday = localStorage.getItem("ppday");
-    let user = JSON.parse(localStorage.getItem("user"));
-    let pickup = user.pickup;
-    let dropoff = user.dropoff;
-    let pickdate = toDate(pickup);
-    let dropdate = toDate(dropoff);
-    let days = (dropdate - pickdate) / (1000 * 60 * 60 * 24);
-   
-    if (days > 14) {
-        ppday = ppday * .9;
-    }
-    let finalprice = ppday * days;
-    localStorage.setItem("days", days);
-    localStorage.setItem("finalprice", finalprice);
-    console.log("Final Price Working");
-}   
-
- function showMess(){
-    let user = JSON.parse(localStorage.getItem("user"));
-    let type = localStorage.getItem("type");
-    let ppday = localStorage.getItem("ppday");
-    let firstname = user.firstname;
-    let lastname = user.lastname;
-    let fullname = firstname + " " + lastname;
-    let days = localStorage.getItem("days");
-    let finalprice = localStorage.getItem("finalprice");
-    let type2 =  type;
-    document.getElementById("fullname").innerHTML = fullname + "!";
-    console.log(fullname);
-    document.getElementById("car").innerHTML = type;
-    document.getElementById("car2").innerHTML = type2;
-
-    document.getElementById("days").innerHTML = days;
-    document.getElementById("ppday").innerHTML = ppday;
-    document.getElementById("total").innerHTML = finalprice;
-    document.querySelector("#pageshow").removeAttribute("hidden");
-    if (days > 14){
-        document.getElementById("discount").innerHTML = "You got a discount of 10%!";
-    }
-    console.log("Show Mess Working");
-   
- }
-
- function pageLoad (){
-    if (window.location.pathname == "/LocalStorageJS/results.html") {
-        processResult();
-        console.log("Page Load Working");
-    } else {
-        localStorage.clear();
-        console.log ("This is not results... Clearing storage");
-    }
- }
-
-window.addEventListener("load", pageLoad);
---
+++ Web Storage App/pizza.js
@@ -0,0 +1,124 @@
+
+function doAll(e) {
+    localStorage.clear();
+    e.preventDefault();
+    pizzaSize();
+    toppingStuff();
+    drinkStuff();
+    totalPrice();
+}
+function pizzaSize() {
+    
+let psize = document.getElementsByName("size");
+for (i = 0; i < psize.length; i++) {
+    if (psize[i].checked) {
+        psize = psize[i].value;
+        localStorage.setItem("size", psize);
+        
+    }
+}
+ switch (psize) {
+     case "small":
+        pizprice = 10;
+        type = "Small:";
+        break;
+     case "medium":
+        pizprice = 15;
+        type = "Medium:";
+        break;
+     case "large":
+        pizprice = 20;
+        type = "Large:";
+ }
+    let display = (type + " " + "$" + pizprice);
+    localStorage.setItem("sizeDisp", display);
+    localStorage.setItem("pizprice", pizprice);
+    document.getElementById('size').innerHTML = display;
+}
+
+function toppingStuff(){
+   let checked = document.querySelectorAll('input[name="toppings"]:checked');
+   let topping = [];
+   checked.forEach(c => {
+      topping.push(c.value);
+   })
+   if (!topping.length > 0) {
+      alert("Select at least one topping!");
+      return false;
+   } 
+
+   let Cheese = topping.includes("cheese");
+   console.log(typeof Cheese);
+   let Pepperoni = topping.includes("pepperoni");
+   let Sausage = topping.includes("sausage");
+   let Olives = topping.includes("olives");
+   let Anchovies = topping.includes("anchovies");
+   let Mushrooms = topping.includes("mushrooms");
+   let topprice = 0;
+   
+   if (Cheese == true) {
+       console.log("Cheese");
+       document.getElementById('cheesetot').innerHTML = "Cheese - $1 ";
+       topprice = topprice + 1;
+
+   } if (Pepperoni == true) {
+       console.log("Pepperoni");
+       document.getElementById('pepperonitot').innerHTML = "Pepperoni - $2 ";
+       topprice = topprice + 2;
+   } if (Sausage == true) {
+       console.log("Sausage");
+       document.getElementById('sausagetot').innerHTML = "Sausage - $4 ";
+       topprice = topprice + 4;
+   }  if (Olives == true) {
+       console.log("Olives");
+       document.getElementById('olivestot').innerHTML = "Olives - $3 ";
+       topprice = topprice + 3;
+   } if (Anchovies == true) {
+       console.log("Anchovies");
+       document.getElementById('anchoviestot').innerHTML = "Anchovies - $5 ";
+       topprice = topprice + 5;
+   } if (Mushrooms == true) {
+       console.log("Mushrooms");
+       document.getElementById('mushroomstot').innerHTML = "Mushrooms - $6 ";
+       topprice = topprice + 6;
+   }
+console.log(topprice);
+localStorage.setItem("topprice", topprice);
+
+
+
+   
+}
+
+function drinkStuff(){
+    let sodacan = document.getElementById("sodacan").value;
+    let soda20 = document.getElementById("soda20").value;
+    let water20 = document.getElementById("water20").value;
+    let lemonade20 = document.getElementById("lemonade20").value;
+
+    let sodaprice = sodacan * 1;
+    let soda20price = soda20 * 2;
+    let water20price = water20 * 1;
+    let lemonade20price = lemonade20 * 2;
+
+    let drtotal = sodaprice + soda20price + water20price + lemonade20price;
+    localStorage.setItem("drtotal", drtotal);
+    document.getElementById("drinkstot").innerHTML = "Drinks: $" + drtotal;
+}
+
+function totalPrice (){
+    let drtotal = localStorage.getItem("drtotal");
+    drtotal = parseFloat(drtotal);
+
+    let piztotal = localStorage.getItem("pizprice");
+    piztotal = parseFloat(piztotal);
+
+    let topprice = localStorage.getItem("topprice");
+    topprice = parseFloat(topprice);
+    let tax = 0.07;
+    let total = parseFloat(drtotal + piztotal + topprice);
+    let taxtotal = total * tax;
+    total = total + taxtotal;
+    document.getElementById("tax").innerHTML = "Tax: $" + taxtotal.toPrecision(2);
+    document.getElementById("subtotal").innerHTML = "Price After Tax: $" + total.toPrecision(2);
+}
